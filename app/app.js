f0be18f1b8e10df3c4fe5c1a4045d44117282a61
Michael Reeder <Michael.Reeder@inin.com>
2015-02-14 18:28:23 -0500

    completed display lesson

--- app/app.js
+++ app/app.js
@@ -23,7 +23,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.use('/', routes);
-app.use('/examples', routes);
+// app.use('/examples', routes);
+// app.use('/university', routes);
+// app.use('/university/:id', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- app/routes/index.js
+++ app/routes/index.js
@@ -10,4 +10,12 @@ router.get('/examples', function(req, res, next) {
     res.render('examples');
 });
 
+router.get('/university', function(req, res, next) {
+    res.render('university');
+});
+
+router.get('/university/:id', function(req, res, next) {
+    res.render('famous-template', { example: req.params.id});
+});
+
 module.exports = router;
